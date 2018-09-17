a47bb86925194b77804dd96fa42bb6ac34564136
freeweed.m@gmail.com <freeweed.m@gmail.com>
2018-09-18 01:16:36 +0700

    edit readme

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ I spend a week for try to implement pan and zoom into ionic 3.
 I try a lot of Library, Finally I find solutions.
 In this repo is my solutions. Please feel free for download, use or modified it.
 
-## Test with div and image
+## Usage
+- `npm install`
+- `ionic serve`
 
 ## Usage
 - pinch for zoom in and zoom out
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { DirectivesModule } from '../directives/directives.module';
-import { Ng2PanZoomModule } from 'ng2-panzoom';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -15,7 +14,6 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    Ng2PanZoomModule,
     DirectivesModule,
     IonicModule.forRoot(MyApp)
   ],
